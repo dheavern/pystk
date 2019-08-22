ac7db89698f8f884a9a06f9d68d9d96e419a13d1
philkr <philkr@users.noreply.github.com>
2019-08-22 16:55:44 -0500

    WIP

--- setup.py
+++ setup.py
@@ -72,46 +72,6 @@ class CMakeBuild(build_ext):
                               cwd=self.build_temp)
 
 
-class FetchDataCommand(Command):
-    description = "fetch the supertuxkart assets"
-    user_options = [('force', 'r', 'forcibly fetch the data (delete existing assets)')]
-    boolean_options = ['force']
-
-    def initialize_options(self):
-        self.force = 0
-
-    def finalize_options(self, ):
-        pass
-
-    def run(self):
-        import io, requests, zipfile
-        assets_dir = os.path.join(this_directory, 'stk-assets')
-        if not os.path.exists(assets_dir) or self.force:
-            r = requests.get(ASSET_URL, stream=True)
-            total_length = r.headers.get('content-length')
-            content = b''
-            if total_length is None:
-                content = r.content
-            else:
-                dl = 0
-                total_length = int(total_length)
-                print("Fetching assets")
-                for data in r.iter_content(1 << 20):
-                    dl += len(data)
-                    content += data
-                    done = int(50 * dl / total_length)
-                    sys.stdout.write("\r[%s%s] %3d%%" % ('=' * done, ' ' * (50 - done), 100 * dl / total_length))
-                print()
-            z = zipfile.ZipFile(io.BytesIO(content))
-            try:
-                shutil.rmtree(assets_dir)
-            except FileNotFoundError:
-                pass
-            z.extractall(assets_dir)
-        else:
-            print("Using existing assets (overwrite with '--force')")
-
-
 class BuildAndCopyData(build_py):
     description = "build_py and copy the supertuxkart assets"
 
@@ -125,7 +85,7 @@ class BuildAndCopyData(build_py):
         except FileNotFoundError:
             pass
         data_files = []
-        for base in ['data/']: # , 'stk-assets/'
+        for base in ['data/']:
             base = os.path.join(this_directory, base)
             for root, dirs, files in os.walk(base):
                 target_dir = os.path.join(self.build_lib, self.packages[0], 'data', root[len(base):])
@@ -136,8 +96,6 @@ class BuildAndCopyData(build_py):
                 for f in files:
                     if '.py' not in f:
                         self.copy_file(os.path.join(root, f), os.path.join(target_dir, f))
-
-    sub_commands = [('fetch_data', lambda x: True)]
 
 
 with open("README.md", "r") as fh:
@@ -171,14 +129,14 @@ setup(
  
     # tell setuptools that all packages will be under the 'src' directory
     # and nowhere else
-    packages=['.'],
-    package_dir={'': '.'},
-    package_data={'': ['*.md', 'docs/*', 'examples/*.py']},
+    packages=['pystk_data'],
+    package_dir={'pystk_data': '.'},
+    package_data={'pystk_data': []},
     # TODO: Add more
     #install_requires=['cmake'],
     python_requires='>=3.6',
     ext_modules=[CMakeExtension('pystk_cpp')],
     # add custom build_ext command
-    cmdclass=dict(fetch_data=FetchDataCommand, build_py=BuildAndCopyData, build_ext=CMakeBuild),
+    cmdclass=dict(build_py=BuildAndCopyData, build_ext=CMakeBuild),
     zip_safe=False,
 )
